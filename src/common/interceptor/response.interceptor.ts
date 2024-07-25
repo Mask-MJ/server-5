9b6af18456b33c6ce65a0a8aa92df2399a9dd93a
mask <momo617473294@gmail.com>
2024-07-25 22:07:42 +0800

    c

--- src/common/interceptor/response.interceptor.ts
+++ src/common/interceptor/response.interceptor.ts
@@ -45,6 +45,7 @@ function changeTime(data: any) {
         data[key] = data[key].map((item: any) => changeTime(item));
       }
       if (data[key] instanceof Date) {
+        console.log('key', key);
         data[key] = dayjs(data[key]).format('YYYY-MM-DD HH:mm:ss');
       }
     }
